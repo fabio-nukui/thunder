095d1dc53aae1653daf862e3dcf0e38ee335b21c
Fabio Nukui <fabio.nukui@gmail.com>
2021-09-21 14:28:21 +0000

    refactored set default, added asserts to secrets

--- src/auth_secrets.py
+++ src/auth_secrets.py
@@ -4,10 +4,16 @@ import utils
 
 def hd_wallet(secret_name: str = configs.SECRET_NAME_MNEMONIC) -> dict:
     secret = utils.aws.get_secret(secret_name, decode_json=True)
-    secret['account'] = int(secret.get('account', 0))
+    assert 'mnemonic' in secret
+
+    secret.setdefault('account', 0)
 
     return secret
 
 
 def binance_api(secret_name: str = configs.SECRET_NAME_BINANCE_KEY) -> dict:
-    return utils.aws.get_secret(secret_name, decode_json=True)
+    secret = utils.aws.get_secret(secret_name, decode_json=True)
+    assert 'api_key' in secret
+    assert 'api_secret' in secret
+
+    return secret
