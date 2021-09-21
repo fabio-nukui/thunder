60256266382d8915e2e3e73f08751f68af2086f0
Fabio Nukui <fabio.nukui@gmail.com>
2021-09-21 17:43:56 +0000

    fix docker lab container

--- docker/jupyter_lab_config.py
+++ docker/jupyter_lab_config.py
@@ -1,8 +1,9 @@
 # flake8: noqa
+SSL_FILES_DIR = '/home/thunder/work/docker'
 c.ExtensionApp.open_browser = False
 c.ServerApp.allow_origin = '*'
 c.ServerApp.allow_password_change = False
-c.ServerApp.certfile = '/home/flash/work/docker/jupyter-server.pem'
+c.ServerApp.certfile = f'{SSL_FILES_DIR}/jupyter-server.pem'
 c.ServerApp.ip = '*'
-c.ServerApp.keyfile = '/home/flash/work/docker/jupyter-server.key'
+c.ServerApp.keyfile = f'{SSL_FILES_DIR}/jupyter-server.key'
 c.ServerApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$1GnzWJ6NTZPmQXkDt3rO2A$JaSJFBZh+NXWbIDp3ApD+g'
