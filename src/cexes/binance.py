84c5ce9c6fe46fce0faa3a869ce9c8f6255b43f0
Fabio Nukui <fabio.nukui@gmail.com>
2021-09-27 05:15:27 -0300

    fix access to PairManager if it is not ready yet; refactored TradingPair init

--- src/cexes/binance.py
+++ src/cexes/binance.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 import atexit
 import time
 from decimal import Decimal
+from threading import Lock
 
 import binance
-from binance.depthcache import DepthCache
+from binance.depthcache import DepthCache, ThreadedDepthCacheManager
 
 import auth_secrets
 from common.token import Token, TokenAmount
 from exceptions import InsufficientLiquidity
 
 DEFAULT_PRECISION = 8
+DEPTH_CACHE_LIMIT = 1000
+DEPTH_CACHE_REFRESH_INTERVAL = 1800
 
 
 class BinanceToken(Token):
@@ -25,7 +28,12 @@ class BinanceTokenAmount(TokenAmount):
 
 
 class TradingPair:
-    def __init__(self, symbol: str, client: binance.Client) -> None:
+    def __init__(
+        self,
+        symbol: str,
+        client: binance.Client,
+        dcm: binance.ThreadedDepthCacheManager,
+    ) -> None:
         self.symbol = symbol
 
         self.bids: list[tuple[Decimal, Decimal]] = []
@@ -42,6 +50,10 @@ class TradingPair:
         self.lot_min_size = self._get_filter_value(info['filters'], 'LOT_SIZE', 'minQty')
         self.min_notional = self._get_filter_value(info['filters'], 'MIN_NOTIONAL', 'minNotional')
 
+        self._ready = False
+        self._ready_lock = Lock()
+        self._start_depth_cache(dcm)
+
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}({self.symbol})'
 
@@ -55,6 +67,9 @@ class TradingPair:
     def update_from_depth_cache(self, depth_cache: DepthCache):
         self.bids = depth_cache.get_bids()
         self.asks = depth_cache.get_asks()
+        if not self._ready:
+            with self._ready_lock:
+                self._ready = True
 
     def get_market_amount_out(self, amount_in: BinanceTokenAmount) -> BinanceTokenAmount:
         amount_out = Decimal(0)
@@ -81,6 +96,21 @@ class TradingPair:
         else:
             raise TypeError(f'{amount_in.token=} not in pair')
 
+    def _start_depth_cache(self, dcm: ThreadedDepthCacheManager):
+        dcm.start_depth_cache(
+            self.update_from_depth_cache,
+            self.symbol,
+            refresh_interval=DEPTH_CACHE_REFRESH_INTERVAL,
+            limit=DEPTH_CACHE_LIMIT,
+            conv_type=Decimal,
+        )
+        for _ in range(100):
+            with self._ready_lock:
+                if self._ready:
+                    return
+            time.sleep(0.1)
+        raise Exception('Timeout on DepthCacheManager initialization')
+
 
 class BinanceClient:
     def __init__(self, api_key: str = None, api_secret: str = None):
@@ -90,29 +120,25 @@ class BinanceClient:
             api_secret = binance_secret['api_secret']
 
         self.client = binance.Client(api_key, api_secret)
-        self.dcm = binance.ThreadedDepthCacheManager(api_key, api_secret)
+        self.dcm = ThreadedDepthCacheManager(api_key, api_secret)
         self._pairs: dict[str, TradingPair] = {}
 
         self.dcm.start()
         atexit.register(self.dcm.stop)  # type: ignore
+        self._wait_dcm_ready()
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}'
 
     def get_trading_pair(self, symbol: str) -> TradingPair:
         if symbol not in self._pairs:
-            pair = TradingPair(symbol, self.client)
-            self._start_depth_cache(pair, symbol)
-            self._pairs[symbol] = pair
+            self._pairs[symbol] = TradingPair(symbol, self.client, self.dcm)
 
         return self._pairs[symbol]
 
-    def _start_depth_cache(self, pair: TradingPair, symbol: str):
-        n_tries = 1000
-        for _ in range(n_tries):
-            if self.dcm._client is None:
-                time.sleep(0.001)
-            else:
-                self.dcm.start_depth_cache(pair.update_from_depth_cache, symbol, conv_type=Decimal)
+    def _wait_dcm_ready(self):
+        for _ in range(2000):
+            if self.dcm._client is not None:
                 return
+            time.sleep(0.001)
         raise Exception('Timeout on ThreadedDepthCacheManager initialization')
