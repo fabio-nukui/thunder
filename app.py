aabfb47abfff33475c2dac1ac8d772be99ed9c6e
Fabio Nukui <fabio.nukui@gmail.com>
2021-10-03 07:36:19 +0000

    added strategy runner

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-import startup
-from cexes.binance import BinanceClient
-from chains.evm import BSCClient, EthereumClient
-from chains.terra import TerraClient
+import importlib
+import logging
+
+import configs
+from startup import setup
+
+log = logging.getLogger(__name__)
+
+GIT_COMMIT = open('git_commit').read().strip()
 
 
 def main():
-    terra_client = TerraClient()
-    bsc_client = BSCClient()
-    ethereum_client = EthereumClient()
-
-    binance_client = BinanceClient()
-    print(terra_client, bsc_client, ethereum_client, binance_client)
+    strategy = importlib.import_module(f'strategies.{configs.STRATEGY}')
+    log.info(f'Running on git commit {GIT_COMMIT}')
+    while True:
+        try:
+            log.info(f'Starting strategy {configs.STRATEGY}')
+            strategy.run()  # type: ignore
+        except Exception as e:
+            log.error('Error during strategy execution')
+            log.exception(e)
+            log.info('Restarting strategy')
 
 
 if __name__ == '__main__':
-    startup.setup()
+    setup()
     main()
