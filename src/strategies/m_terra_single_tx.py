313b9cfdac1580f3dbbd2d46b19161ac5a7cdcf1
Fabio Nukui <fabio.nukui@gmail.com>
2021-10-18 22:00:57 +0000

    changed order of strategies in meta-strategy

--- src/strategies/m_terra_single_tx.py
+++ src/strategies/m_terra_single_tx.py
@@ -9,20 +9,20 @@ from .common.terra_single_tx_arbitrage import TerraSingleTxArbitrage, run_strate
 
 async def run(max_n_blocks: int = None):
     async with await TerraClient.new() as client:
-        s_lp_tower_arbs, luna_market_arbs, s_ust_cycles_arbs = await asyncio.gather(
-            s_lp_tower.get_arbitrages(client),
+        luna_market_arbs, s_ust_cycles_arbs, s_lp_tower_arbs = await asyncio.gather(
             s_luna_ust_market.get_arbitrages(client),
             s_ust_cycles.get_arbitrages(client),
+            s_lp_tower.get_arbitrages(client),
         )
-        s_lp_tower_filters = s_lp_tower.get_filters(s_lp_tower_arbs)
         luna_market_filters = s_luna_ust_market.get_filters(luna_market_arbs)
         s_ust_cycles_filters = s_ust_cycles.get_filters(s_ust_cycles_arbs)
+        s_lp_tower_filters = s_lp_tower.get_filters(s_lp_tower_arbs)
 
         arb_routes: Sequence[TerraSingleTxArbitrage] = [
-            *s_lp_tower_arbs,
             *luna_market_arbs,
             *s_ust_cycles_arbs,
+            *s_lp_tower_arbs,
         ]
-        mempool_filters = s_lp_tower_filters | luna_market_filters | s_ust_cycles_filters
+        mempool_filters = luna_market_filters | s_ust_cycles_filters | s_lp_tower_filters
 
         await run_strategy(client, arb_routes, mempool_filters, max_n_blocks)
