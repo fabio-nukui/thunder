3fed0b24a170699b210c973aa118ac3e8b5c0194
Fabio Nukui <fabio.nukui@gmail.com>
2021-10-26 00:31:41 +0000

    increased min profit

--- src/strategies/common/default_params.py
+++ src/strategies/common/default_params.py
@@ -2,6 +2,6 @@ from chains.terra import UST
 
 MAX_SINGLE_ARBITRAGE_AMOUNT = UST.to_amount(50_000)
 MAX_N_REPEATS = 20
-MIN_PROFIT_UST = UST.to_amount(2)
+MIN_PROFIT_UST = UST.to_amount(5)
 MIN_START_AMOUNT = UST.to_amount(50)
 OPTIMIZATION_TOLERANCE = UST.to_amount("0.01")
