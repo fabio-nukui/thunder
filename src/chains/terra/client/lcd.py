22e73b843e4a036da1874393ccd6f3292baf0e02
Fabio Nukui <fabio.nukui@gmail.com>
2021-10-29 04:28:04 +0000

    added some controls to lcd client

--- src/chains/terra/client/lcd.py
+++ src/chains/terra/client/lcd.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import aiohttp
@@ -6,24 +7,38 @@ from terra_sdk.client.lcd import AsyncLCDClient
 
 log = logging.getLogger(__name__)
 
+MAX_CONCURRENT_REQUESTS = 20
+
 
 class AsyncLCDClient2(AsyncLCDClient):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._requests_semaphore = asyncio.Semaphore(MAX_CONCURRENT_REQUESTS)
+        self._reconnect_lock = asyncio.Lock()
+
     async def _get(self, *args, **kwargs):
         try:
-            return await super()._get(*args, **kwargs)
+            async with self._requests_semaphore:
+                return await super()._get(*args, **kwargs)
         except ServerDisconnectedError:
             await self._reconnect_session()
             return await super()._get(*args, **kwargs)
 
     async def _post(self, *args, **kwargs):
         try:
-            return await super()._post(*args, **kwargs)
+            async with self._requests_semaphore:
+                return await super()._post(*args, **kwargs)
         except ServerDisconnectedError:
             await self._reconnect_session()
             return await super()._post(*args, **kwargs)
 
     async def _reconnect_session(self):
-        log.debug("Reconnecting LCD session")
-        old_session = self.session
-        self.session = aiohttp.ClientSession(headers={"Accept": "application/json"}, loop=self.loop)
-        await old_session.close()
+        async with self._reconnect_lock:
+            if not self.session.closed:
+                return
+            log.debug("Reconnecting LCD session")
+            old_session = self.session
+            self.session = aiohttp.ClientSession(
+                headers={"Accept": "application/json"}, loop=self.loop
+            )
+            await old_session.close()
