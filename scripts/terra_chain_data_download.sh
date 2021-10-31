76e625d019147662ae1e05c25f9bffaa5961359b
Fabio Nukui <fabio.nukui@gmail.com>
2021-10-31 04:45:50 +0000

    refactored download/upload scripts to pipe results directly to/from S3; removed live_upload script

--- scripts/terra_chain_data_download.sh
+++ scripts/terra_chain_data_download.sh
@@ -6,30 +6,21 @@ if [[ $(id -u) -ne 0 ]]; then
    exit 1
 fi
 
-TERRA_DATA_DIR=/mnt/nvme0/terra/data
-S3_PATH=s3://crypto-thunder/chain_data/terra/
+TERRA_DIR=/mnt/nvme0/terra
+S3_PATH=s3://crypto-thunder/chain_data/terra
 RUNNER_USER=ubuntu
-CUR_DIR=$PWD
-
-snapshot_name=$(aws s3 ls $S3_PATH | awk -F ' ' '{print $4}' | tail -n 1)
-
-sudo -i -u "$RUNNER_USER" bash << EOF
-set -eu; cd $CUR_DIR
-echo Downloading latest snapshot "$snapshot_name"
-aws s3 cp $S3_PATH"$snapshot_name" .
-EOF
 
 echo Stopping terrad
 systemctl stop terrad
+sleep 10
+
+SNAPSHOT_NAME=$(aws s3 ls $S3_PATH | awk -F ' ' '{print $4}' | tail -n 1)
 
 sudo -i -u "$RUNNER_USER" bash << EOF
-set -eu; cd $CUR_DIR
-echo Decompressing "$snapshot_name"
-rm -rf $TERRA_DATA_DIR
-mkdir $TERRA_DATA_DIR
-pv "$snapshot_name" | tar --use-compress-program=pigz -C $TERRA_DATA_DIR -xf -
-rm "$snapshot_name"
+set -eu
+rm -rf "$TERRA_DIR"/data
+aws s3 cp "$S3_PATH"/"$SNAPSHOT_NAME" - | pv | tar --use-compress-program=pigz -xC "$TERRA_DIR"
 EOF
 
-echo Starting terrad
+echo Restating terrad
 systemctl start terrad
--- scripts/terra_chain_data_upload_stop.sh
+++ scripts/terra_chain_data_upload_stop.sh
@@ -6,24 +6,20 @@ if [[ $(id -u) -ne 0 ]]; then
    exit 1
 fi
 
-TERRA_DATA_DIR=/mnt/nvme0/terra/data
-S3_PATH=s3://crypto-thunder/chain_data/terra/
+TERRA_DIR=/mnt/nvme0/terra
+S3_PATH=s3://crypto-thunder/chain_data/terra
 RUNNER_USER=ubuntu
-CUR_DIR=$PWD
 
 echo Stopping terrad
 systemctl stop terrad
 sleep 10
 
-FILE_NAME=terra-data-$(date -u +%Y-%m-%dT%H-%M).tar.gz
+SNAPSHOT_NAME=terra-data-$(date -u +%Y-%m-%dT%H-%M).tar.gz
 
 sudo -i -u "$RUNNER_USER" bash << EOF
-set -eu; cd $CUR_DIR
-echo Compressing data to $FILE_NAME
-tar --use-compress-program='pigz --recursive | pv' -cf $FILE_NAME data
-echo uploading to S3
-aws s3 cp $FILE_NAME $S3_PATH
-rm $FILE_NAME
+set -eu
+cd "$TERRA_DIR"
+tar --use-compress-program='pigz --recursive | pv' -cf - data | aws s3 cp - "$S3_PATH"/"$SNAPSHOT_NAME"
 EOF
 
 echo Restating terrad
