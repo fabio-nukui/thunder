91d576965dca4fec88fe5186abb84824cf8d0f6b
Fabio Nukui <fabio.nukui@gmail.com>
2021-11-10 02:08:19 +0000

    updated cached decorator to avoid calling a function again while it is being awaited

--- src/utils/cache/asyncache.py
+++ src/utils/cache/asyncache.py
@@ -1,10 +1,15 @@
 """
 Adapted from https://github.com/hephex/asyncache/blob/master/asyncache/__init__.py
 """
+import asyncio
 import functools
 import inspect
 
 from cachetools import keys
+
+
+class _FetchEvent(asyncio.Event):
+    pass
 
 
 def cached(cache, key=keys.hashkey, lock=None):
@@ -25,15 +30,28 @@ def cached(cache, key=keys.hashkey, lock=None):
                 async def async_wrapper_no_lock(*args, **kwargs):
                     k = key(*args, **kwargs)
                     try:
-                        return cache[k]
+                        val = cache[k]
+                        if type(val) is _FetchEvent:
+                            await val.wait()
+                            return cache[k]
+                        else:
+                            return val
                     except KeyError:
                         pass  # key not found
 
-                    val = await func(*args, **kwargs)
+                    event = _FetchEvent()
+                    cache[k] = event
+                    try:
+                        val = await func(*args, **kwargs)
+                    except Exception:
+                        del cache[k]
+                        event.set()
+                        raise
                     try:
                         cache[k] = val
-                    except ValueError:
-                        pass  # val too large
+                    except ValueError:  # val too large
+                        del cache[k]
+                    event.set()
                     return val
 
                 return functools.update_wrapper(async_wrapper_no_lock, func)
@@ -41,17 +59,31 @@ def cached(cache, key=keys.hashkey, lock=None):
             async def async_wrapper_lock(*args, **kwargs):
                 k = key(*args, **kwargs)
                 try:
-                    async with lock:
-                        return cache[k]
+                    val = cache[k]
+                    if type(val) is _FetchEvent:
+                        await val.wait()
+                        async with lock:
+                            return cache[k]
+                    else:
+                        return val
                 except KeyError:
                     pass  # key not found
-
-                val = await func(*args, **kwargs)
+                event = _FetchEvent()
+                async with lock:
+                    cache[k] = event
                 try:
+                    val = await func(*args, **kwargs)
+                except Exception:
                     async with lock:
+                        del cache[k]
+                    event.set()
+                    raise
+                async with lock:
+                    try:
                         cache[k] = val
-                except ValueError:
-                    pass  # val too large
+                    except ValueError:  # val too large
+                        del cache[k]
+                event.set()
                 return val
 
             return functools.update_wrapper(async_wrapper_lock, func)
