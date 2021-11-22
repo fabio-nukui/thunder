ee2f8dab17e52f113488d7f47b73e6e81c6a046f
Fabio Nukui <fabio.nukui@gmail.com>
2021-11-22 11:38:24 +0000

    changed default params

--- src/strategies/common/default_params.py
+++ src/strategies/common/default_params.py
@@ -2,6 +2,6 @@ from chains.cosmos.terra import UST
 
 MAX_SINGLE_ARBITRAGE_AMOUNT = UST.to_amount(50_000)
 MAX_N_REPEATS = 20
-MIN_PROFIT_UST = UST.to_amount(20)
-MIN_START_AMOUNT = UST.to_amount(200)
+MIN_PROFIT_UST = UST.to_amount(10)
+MIN_START_AMOUNT = UST.to_amount(50)
 OPTIMIZATION_TOLERANCE = UST.to_amount(1)
