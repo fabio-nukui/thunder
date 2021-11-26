de676bc9a024a6d9f2fcd155cd9fea33f969bba9
Fabio Nukui <fabio.nukui@gmail.com>
2021-11-26 11:41:02 +0000

    add app files to import tests

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,10 +1,19 @@
 import importlib
+import os
+import sys
 from glob import glob
 
 import pytest
 
+sys.path.append(os.getcwd())
 
-@pytest.mark.parametrize("file", (path[4:] for path in glob("src/**/*.py", recursive=True)))
+import_files = [
+    *(path[4:] for path in glob("src/**/*.py", recursive=True)),
+    *glob("app/*.py"),
+]
+
+
+@pytest.mark.parametrize("file", import_files)
 def test_imports(file: str):
     module_name = file.replace("/", ".")[:-3]
     importlib.import_module(module_name)
