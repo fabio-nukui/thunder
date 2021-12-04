c298b89ce1d37b2f1ff5ddf55eff9ec5ed243aca
Fabio Nukui <fabio.nukui@gmail.com>
2021-12-04 03:36:19 +0000

    reduce minimum start amount

--- src/strategies/common/default_params.py
+++ src/strategies/common/default_params.py
@@ -3,5 +3,5 @@ from chains.cosmos.terra import UST
 MAX_SINGLE_ARBITRAGE_AMOUNT = UST.to_amount(50_000)
 MAX_N_REPEATS = 20
 MIN_PROFIT_UST = UST.to_amount(10)
-MIN_START_AMOUNT = UST.to_amount(50)
+MIN_START_AMOUNT = UST.to_amount(20)
 OPTIMIZATION_TOLERANCE = UST.to_amount(1)
