17128037cf7c158b178ce7a4b9cf61fcb793a35d
Fabio Nukui <fabio.nukui@gmail.com>
2021-12-07 16:41:12 +0000

    increased max possible arbitrage to 5M

--- src/strategies/common/default_params.py
+++ src/strategies/common/default_params.py
@@ -1,7 +1,7 @@
 from chains.cosmos.terra import UST
 
-MAX_SINGLE_ARBITRAGE_AMOUNT = UST.to_amount(100_000)
-MAX_N_REPEATS = 20
+MAX_SINGLE_ARBITRAGE_AMOUNT = UST.to_amount(200_000)
+MAX_N_REPEATS = 25
 MIN_PROFIT_UST = UST.to_amount(10)
 MIN_START_AMOUNT = UST.to_amount(20)
 OPTIMIZATION_TOLERANCE = UST.to_amount(1)
